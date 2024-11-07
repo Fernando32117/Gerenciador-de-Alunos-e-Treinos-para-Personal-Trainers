80dfcdd5179627b54fb73799016ce97d95bdc071
Francisco Fernando <nando32117@gmail.com>
2024-11-06 22:05:10 -0300

    alteracao e adicao de funcoes e arquivos

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-document.getElementById("formPersonal").addEventListener("submit", function (e) {
+document.getElementById("formCadastroPersonal").addEventListener("submit", function (e) {
   e.preventDefault(); // Previne o comportamento padrão do formulário  
 
-  const nome = document.getElementById("nome").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  const nomePersonal = document.getElementById("nomePersonal").value;
+  const emailPersonal = document.getElementById("emailPersonal").value;
+  const passwordPersonal = document.getElementById("passwordPersonal").value;
   const cref = document.getElementById("cref").value;
 
   // Enviar os dados para a API  
@@ -12,21 +12,60 @@ document.getElementById("formPersonal").addEventListener("submit", function (e) 
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ nome, email, password, cref })
+    body: JSON.stringify({ nomePersonal, emailPersonal, passwordPersonal, cref })
+  })
+    .then(response => {
+      if (!response.ok) {
+        return response.json().then(error => { throw new Error(error.message); });
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Sucesso:', data);
+      alert('Personal cadastrado com sucesso! ID: ' + data.id);
+      window.location.href = "dashboardPersonal.html";
+    })
+    .catch((error) => {
+      console.error('Erro:', error);
+      alert('Usuário com ' + error.message);
+    });
+});
+
+
+document.getElementById("formLoginPersonal").addEventListener("submit", function (e) {
+  e.preventDefault(); // Previne o comportamento padrão do formulário
+
+  const emailPersonal = document.getElementById("emailPersonal").value;
+  const passwordPersonal = document.getElementById("passwordPersonal").value;
+
+  // Enviar os dados para a API de login
+  fetch('http://localhost:3000/loginPersonal', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ emailPersonal, passwordPersonal })
   })
     .then(response => response.json())
     .then(data => {
-      console.log('Sucesso:', data);
-      alert('Usuário cadastrado com sucesso! ID: ' + data.id);
-      window.location.href = "dashboardPersonal.html";
+      if (data.message === 'Login bem-sucedido') {
+        console.log('Sucesso:', data);
+        alert('Login bem-sucedido! Bem-vindo, ' + data.user.nomePersonal);
+        window.location.href = "dashboardPersonal.html";
+      } else {
+        alert('Erro: ' + data.message);
+      }
     })
     .catch((error) => {
       console.error('Erro:', error);
     });
 });
 
+
+
+
 document.getElementById("formCadastroAluno").addEventListener("submit", function (e) {
-  e.preventDefault(); // Previne o comportamento padrão do formulário  
+  e.preventDefault(); // Previne o comportamento padrão do formulário
 
   const nomeAluno = document.getElementById("nomeAluno").value;
   const generoAluno = document.getElementById("generoAluno").value;
@@ -34,15 +73,15 @@ document.getElementById("formCadastroAluno").addEventListener("submit", function
   const alunoPeso = document.getElementById("alunoPeso").value;
   const alunoAltura = document.getElementById("alunoAltura").value;
   const alunoLogin = document.getElementById("alunoLogin").value;
-  const alunoSenha = document.getElementById("alunoSenha").value;
+  const alunoPassword = document.getElementById("alunoPassword").value;
 
-  // Enviar os dados para a API  
-  fetch('http://localhost:3000/cadastroAluno', {
+  // Enviar os dados para a API
+  fetch('http://localhost:3000/usuarioAluno', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ nomeAluno, generoAluno, alunoNascimento, alunoPeso, alunoAltura, alunoLogin, alunoSenha})
+    body: JSON.stringify({ nomeAluno, generoAluno, alunoNascimento, alunoPeso, alunoAltura, alunoLogin, alunoPassword })
   })
     .then(response => response.json())
     .then(data => {
@@ -55,77 +94,74 @@ document.getElementById("formCadastroAluno").addEventListener("submit", function
     });
 });
 
+document.getElementById("formLoginAluno").addEventListener("submit", function (e) {
+  e.preventDefault(); // Previne o comportamento padrão do formulário
 
-
-function handlePersonalLogin(event) {
-  event.preventDefault();
-  const personalEmail = document.getElementById("personalEmail").value;
-  const personalPassword = document.getElementById("personalPassword").value;
-  const personalCref = document.getElementById("personalCref").value;
-  if (personalEmail && personalPassword && personalCref) {
-    alert("Login realizado com sucesso !");
-    window.location.href = "dashboard-personal.html";
-  } else {
-    alert("Por favor, preencha todos os campos!");
-  }
-}
-
-function handleAlunoLogin(event) {
-  event.preventDefault();
-
-  const alunoEmail = document.getElementById("alunoEmail").value;
+  const alunoLogin = document.getElementById("alunoLogin").value;
   const alunoPassword = document.getElementById("alunoPassword").value;
 
-  if (alunoEmail && alunoPassword) {
-    alert("Login realizado com sucesso !");
-    window.location.href = "dashboard-aluno.html";
-  } else {
-    alert("Por favor, preencha todos os campos!");
-  }
-}
+  // Enviar os dados para a API de login
+  fetch('http://localhost:3000/loginAluno', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ alunoLogin, alunoPassword })
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.message === 'Login bem-sucedido') {
+        console.log('Sucesso:', data);
+        alert('Login bem-sucedido! Bem-vindo, ' + data.user.nomeAluno);
+        window.location.href = "dashboardAluno.html";
+      } else {
+        alert('Erro: ' + data.message);
+      }
+    })
+    .catch((error) => {
+      console.error('Erro:', error);
+    });
+});
+
+
 
 function handleCadastroTreino(event) {
-  event.preventDefault();
+  event.preventDefault(); // Previne o comportamento padrão do formulário
 
-  const alunoSelecionado = document.getElementById("aluno").value;
-  const grupoMuscular = document.getElementById("grupo-muscular").value;
+  const aluno = document.getElementById("aluno").value;
+  const grupoMuscular = document.getElementById("grupoMuscular").value;
   const series = document.getElementById("series").value;
   const repeticoes = document.getElementById("repeticoes").value;
   const observacoes = document.getElementById("observacoes").value;
   const gifInput = document.getElementById("gifInput").files[0];
 
-  if (!alunoSelecionado && grupoMuscular && series && repeticoes && observacoes && gifInput) {
-    alert("Por favor, preencha todos os campos.");
-  }
+  const formData = new FormData();
+  formData.append('alunoId', aluno);
+  formData.append('grupoMuscular', grupoMuscular);
+  formData.append('series', series);
+  formData.append('repeticoes', repeticoes);
+  formData.append('observacoes', observacoes);
+  formData.append('gif', gifInput);
 
-  // Aqui você pode implementar a lógica de salvar o treino para o aluno selecionado
-  alert(`Treino cadastrado para o aluno ${alunoSelecionado}!`);
-  window.location.href = "dashboard-personal.html";
+  fetch('http://localhost:3000/cadastroTreinoAluno', {
+    method: 'POST',
+    body: formData
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Sucesso:', data);
+      alert('Treino cadastrado com sucesso! ID: ' + data.aluno);
+      window.location.href = "dashboardPersonal.html";
+    })
+    .catch((error) => {
+      console.error('Erro:', error);
+    });
 }
 
-// function buscarAluno(event) {
-//   const query = event.target.value.toLowerCase();
-//   const listaAlunos = document.getElementById("lista-alunos");
-//   listaAlunos.innerHTML = ""; // Limpa a lista anterior
 
-//   const alunosFiltrados = alunos.filter(aluno => aluno.nome.toLowerCase().includes(query));
-
-//   alunosFiltrados.forEach(aluno => {
-//       const item = document.createElement("div");
-//       item.className = "aluno-item";
-//       item.textContent = aluno.nome;
-//       item.onclick = () => selecionarAluno(aluno);
-//       listaAlunos.appendChild(item);
-//   });
-
-//   listaAlunos.style.display = alunosFiltrados.length ? "block" : "none";
-// }
-
-// function selecionarAluno(aluno) {
-//   document.getElementById("aluno").value = aluno.nome;
-//   document.getElementById("lista-alunos").style.display = "none";
-// }
-
+function showCadastroPersonal() {
+  window.location.href = 'cadastroPersonal.html';
+}
 
 function showCadastroAluno() {
   window.location.href = 'cadastroAluno.html';
--- server.js
+++ server.js
@@ -3,14 +3,6 @@ const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-// Inicializar o servidor Express
-const app = express();
-const port = 3000;
-
-// Middleware para fazer parsing do corpo das requisições (necessário para POST)
-app.use(cors());
-app.use(bodyParser.json());
-
 // Inicializar o banco de dados SQLite
 const db = new sqlite3.Database('./meu_banco.db', (err) => {
   if (err) {
@@ -20,15 +12,25 @@ const db = new sqlite3.Database('./meu_banco.db', (err) => {
   }
 });
 
-// Criar tabela se não existir 
+// Inicializar o servidor Express
+const app = express();
+const port = 3000;
+// Middleware para fazer parsing do corpo das requisições (necessário para POST)
+app.use(cors());
+app.use(bodyParser.json());
+
+
+
+// Criar tabela se não existir usuarioPersonal
 db.run(`CREATE TABLE IF NOT EXISTS usuarioPersonal (  
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  nome TEXT NOT NULL, 
-  email TEXT NOT NULL,  
-  password TEXT NOT NULL,
+  nomePersonal TEXT NOT NULL, 
+  emailPersonal TEXT NOT NULL,  
+  passwordPersonal TEXT NOT NULL,
   cref TEXT NOT NULL
 )`);
 
+// Criar tabela se não existir usuarioAluno
 db.run(`CREATE TABLE IF NOT EXISTS usuarioAluno (  
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   nomeAluno TEXT NOT NULL,  
@@ -40,32 +42,118 @@ db.run(`CREATE TABLE IF NOT EXISTS usuarioAluno (
   alunoSenha TEXT NOT NULL
 )`);
 
+// Criar tabela de treinos se não existir 
+db.run(`CREATE TABLE IF NOT EXISTS treinoAluno ( 
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  alunoId INTEGER NOT NULL, 
+  grupoMuscular TEXT NOT NULL, 
+  series INTEGER NOT NULL, 
+  repeticoes INTEGER NOT NULL, 
+  observacoes TEXT, 
+  gif BLOB, 
+  FOREIGN KEY (alunoId) REFERENCES usuarioAluno(id) )`);
 
-// Criar uma rota POST para o login do personal trainer
+
+// Rota para cadastro do personal trainer
 app.post('/usuarioPersonal', (req, res) => {
+  const { nomePersonal, emailPersonal, passwordPersonal, cref } = req.body;
 
-  const { nome, email, password, cref } = req.body;
-
-  const sql = `INSERT INTO usuarioPersonal (nome, email, password, cref) VALUES (?, ?, ?, ?)`;
-  db.run(sql, [nome, email, password, cref], function (err) {
+  // Verificar se o e-mail já existe
+  const checkEmailSql = `SELECT * FROM usuarioPersonal WHERE emailPersonal = ?`;
+  db.get(checkEmailSql, [emailPersonal], (err, row) => {
     if (err) {
-      return console.error(err.message);
+      return res.status(500).json({ error: err.message });
     }
-    res.json({ id: this.lastID }); // Retorna o ID do novo registro  
+    if (row) {
+      // E-mail já existe
+      return res.status(400).json({ message: 'e-mail já cadastrado' });
+    } else {
+      // E-mail não existe, inserir novo registro
+      const insertSql = `INSERT INTO usuarioPersonal (nomePersonal, emailPersonal, passwordPersonal, cref) VALUES (?, ?, ?, ?)`;
+      db.run(insertSql, [nomePersonal, emailPersonal, passwordPersonal, cref], function (err) {
+        if (err) {
+          return res.status(500).json({ error: err.message });
+        }
+        res.json({ id: this.lastID }); // Retorna o ID do novo registro
+      });
+    }
   });
 });
 
-// Criar uma rota POST para o login do aluno
-app.post('/usuarioAluno', (req, res) => {
+// Criar uma rota POST para autenticação do personal trainer
+app.post('/loginPersonal', (req, res) => {
+  const { emailPersonal, passwordPersonal } = req.body;
 
-  const { nomeAluno, generoAluno, alunoNascimento, alunoPeso, alunoAltura, alunoLogin, alunoSenha } = req.body;
-
-  const sql = `INSERT INTO usuarioAluno (nomeAluno, generoAluno, alunoNascimento, alunoPeso, alunoAltura, alunoLogin, alunoSenha) VALUES (?, ?, ?, ?, ?, ?, ?)`;
-  db.run(sql, [nomeAluno, generoAluno, alunoNascimento, alunoPeso, alunoAltura, alunoLogin, alunoSenha], function (err) {
+  const sql = `SELECT * FROM usuarioPersonal WHERE emailPersonal = ? AND passwordPersonal = ?`;
+  db.get(sql, [emailPersonal, passwordPersonal], (err, row) => {
     if (err) {
-      return console.error(err.message);
+      return res.status(500).json({ error: err.message });
     }
-    res.json({ id: this.lastID }); // Retorna o ID do novo registro  
+    if (row) {
+      // Usuário encontrado, autenticação bem-sucedida
+      res.json({ message: 'Login bem-sucedido', user: row });
+    } else {
+      // Usuário não encontrado, autenticação falhou
+      res.status(401).json({ message: 'Credenciais inválidas' });
+    }
+  });
+});
+
+// Criar uma rota POST para o cadastro do aluno
+app.post('/usuarioAluno', (req, res) => {
+  const { nomeAluno, generoAluno, alunoNascimento, alunoPeso, alunoAltura, alunoLogin, alunoPassword } = req.body;
+
+  // Verificar se o e-mail já existe
+  const checkEmailSql = `SELECT * FROM usuarioAluno WHERE alunoLogin = ?`;
+  db.get(checkEmailSql, [alunoLogin], (err, row) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    if (row) {
+      // E-mail já existe
+      return res.status(400).json({ message: 'E-mail já cadastrado' });
+    } else {
+      // E-mail não existe, inserir novo registro
+      const insertSql = `INSERT INTO usuarioAluno (nomeAluno, generoAluno, alunoNascimento, alunoPeso, alunoAltura, alunoLogin, alunoSenha) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+      db.run(insertSql, [nomeAluno, generoAluno, alunoNascimento, alunoPeso, alunoAltura, alunoLogin, alunoPassword], function (err) {
+        if (err) {
+          return res.status(500).json({ error: err.message });
+        }
+        res.json({ id: this.lastID }); // Retorna o ID do novo registro
+      });
+    }
+  });
+});
+
+// Rota para autenticação do aluno
+app.post('/loginAluno', (req, res) => {
+  const { alunoLogin, alunoSenha } = req.body;
+
+  const sql = `SELECT * FROM usuarioAluno WHERE alunoLogin = ? AND alunoSenha = ?`;
+  db.get(sql, [alunoLogin, alunoSenha], (err, row) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    if (row) {
+      // Usuário encontrado, autenticação bem-sucedida
+      res.json({ message: 'Login bem-sucedido', user: row });
+    } else {
+      // Usuário não encontrado, autenticação falhou
+      res.status(401).json({ message: 'Credenciais inválidas' });
+    }
+  });
+});
+
+// Rota para cadastro de treino do aluno
+app.post('/cadastroTreinoAluno', (req, res) => {
+  const { alunoId, grupoMuscular, series, repeticoes, observacoes, gif } = req.body;
+
+  const sql = `INSERT INTO treinoAluno (alunoId, grupoMuscular, series, repeticoes, observacoes, gif) VALUES (?, ?, ?, ?, ?, ?)`;
+  db.run(sql, [alunoId, grupoMuscular, series, repeticoes, observacoes, gif], function (err) {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json({ id: this.lastID }); // Retorna o ID do novo registro
   });
 });
 
